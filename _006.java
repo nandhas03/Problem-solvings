2dbfc8dd79835155ea8c103c1ffcb6fb96714065
NANDHA KUMAR S <162905089+NANDHA311223@users.noreply.github.com>
2024-08-05 11:06:00 +0530

    _006.java_Loops_1
    Print a 2 Table (N x i = result)

---
+++ _006.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int N =scan.nextInt();
+         
+       for(int i=1;i<=10;i++){
+         System.out.println(N+" x "+i+" = "+i*N);  
+       }
+    }
+}
