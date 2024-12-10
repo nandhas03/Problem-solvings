a2e52256f6309061ccd8715dbd09b86b8c0ed769
Nandha Kumar S <162905089+NANDHA311223@users.noreply.github.com>
2024-12-10 23:55:05 +0530

    Reverse_a_String

---
+++ Basic_Programs/_001.java
@@ -0,0 +1,10 @@
+//Reverse a String in JAVA
+
+class Nandhu{                     
+    public static void main(String[] args){
+ 	  String word = "ABCD";
+    	for(int i=word.length()-1;i>=0;i--){
+    	System.out.print(word.charAt(i));
+       }
+	  }
+	}
