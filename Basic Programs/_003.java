aaaeed4f812b7996d6a8043dbbe8a23f95058617
Nandha Kumar S <162905089+NANDHA311223@users.noreply.github.com>
2024-12-12 20:21:42 +0530

    Create _003.java

---
+++ Basic Programs/_003.java
@@ -0,0 +1,12 @@
+//Swapping two numbers 
+        //(using third variable)
+class Nandhu {
+     public static void main(String[]args) {
+		int a=5;
+		int b=6;
+		int Temp =a;
+		a=b;
+		b=Temp;
+		System.out.println(a+","+b);
+	}
+}
