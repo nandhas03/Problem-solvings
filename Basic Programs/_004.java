321a27a519c9ad036b9de0c454e48a7bb809924a
Nandha Kumar S <162905089+NANDHA311223@users.noreply.github.com>
2024-12-13 20:19:42 +0530

    Create _004.java

---
+++ Basic Programs/_004.java
@@ -0,0 +1,12 @@
+//Swapping two numbers 
+        //(without using third variable)
+public class Nandhu {
+     public static void main(String[]args){
+		int a=5;
+		int b=6;
+	 	a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println(a+","+b);
+	}
+}
