4f9e65836c3887f84212b65957a6168a8f145235
Nandha Kumar S <162905089+NANDHA311223@users.noreply.github.com>
2024-12-18 19:42:18 +0530

    Fibonacci_Series

---
+++ Basic Programs/_009.java
@@ -0,0 +1,10 @@
+//Fibonacci series
+public class Nandhu{
+    public static void main(String[]args){
+		int f=0,s=1,t=0;
+		for(int i=0;i<10;i++){
+		   System.out.println(f);
+		   t=f+s;
+		   f=s;
+	       s=t;
+		}}}
