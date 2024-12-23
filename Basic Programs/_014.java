52ef0a3c3af48e8305c79cd22565f4bfa065c380
Nandha Kumar S <162905089+NANDHA311223@users.noreply.github.com>
2024-12-23 17:52:39 +0530

    Factorial_of_a_given_integer

---
+++ Basic Programs/_014.java
@@ -0,0 +1,11 @@
+//Factorial_of_a_Given_Integer
+public class Nandhu{
+    public static void main(String[]args){
+      int no=5,fact;
+      fact=no;
+      while(no>1){
+            no--;
+    	    fact=fact*no;
+      }
+       System.out.println(fact);
+    }}
