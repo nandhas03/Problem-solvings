afc40186743bed4e5c4494a2f087f847b0155f83
Nandha Kumar S <162905089+NANDHA311223@users.noreply.github.com>
2024-12-24 17:48:12 +0530

    Remove_duplicate_elements_in_an_Array

---
+++ Basic Programs/_015.java
@@ -0,0 +1,11 @@
+//Remove_duplicate_elements_in_an_Array
+public class Nandhu{   
+	public static void main(String[]args){
+          int[]arr= {6,3,7,4,4,9,2,0,8,7};
+
+      for(int i=0;i<arr.length-1;i++){
+      for(int j=i+1;j<arr.length;j++){
+            
+        if(arr[i]==arr[j]){
+          System.out.println(arr[i]);
+    }}}}}
