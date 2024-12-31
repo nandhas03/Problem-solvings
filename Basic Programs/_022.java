a5585c46f80ae06def6ed87cdae0b9892ca24538
Nandha Kumar S <162905089+nandhas03@users.noreply.github.com>
2024-12-31 18:46:02 +0530

    Reverse_word's_in_String

---
+++ Basic Programs/_022.java
@@ -0,0 +1,9 @@
+//Reverse_word's_in_String
+public class Nandhu{
+    public static void main(String[]args){
+ 	    String s ="my name is nandhu";
+        String a[]=s.split(" ");
+   
+    for(int i=a.length-1;i>=0;i--){
+    	System.out.print(a[i]+" ");
+    }}}
