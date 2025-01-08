3816b30ebd631744218a25dcc3daca56a7d0374d
Nandha Kumar S <162905089+nandhas03@users.noreply.github.com>
2025-01-08 22:18:33 +0530

    Inverted_half_pyramid

---
+++ Basic Programs/_029.java
@@ -0,0 +1,10 @@
+//print_inverted_half_pyramid
+public class Nandhu{
+    public static void main(String[] args){
+      int rows=5;
+        for(int i=rows;i>=1;--i){
+        for(int j=1;j<=i;++j){
+         System.out.print("* ");
+         }
+         System.out.println();
+      }}}
