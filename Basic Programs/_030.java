8591f4595c0a1cf51f55c37836e0515aaa661c20
Nandha Kumar S <162905089+nandhas03@users.noreply.github.com>
2025-01-09 20:10:02 +0530

    Print_Floyd's_triangle

---
+++ Basic Programs/_030.java
@@ -0,0 +1,10 @@
+//print Floyd's triangle
+public class Nandhu{   
+	public static void main(String[]args){
+      int i,j,k=1;
+    	for(i=1;i<=5;i++){
+    	for(j=1;j<i+1;j++){
+    	  System.out.print(k++ + " "); 
+    	 }
+    	  System.out.println();
+    	}}}
